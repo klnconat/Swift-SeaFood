f5030d069c83a6a4d2691e2616d9c9bccbd4af4c
Onat Kilinc <okilinc@geosys.com.tr>
2024-10-28 12:08:49 +0300

    Initial Commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Farmlabs Agriculture Tech on 28.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
